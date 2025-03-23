b0f937f4171888440341a8acc4c11db61f272819
Maksym <shalamax11@gmail.com>
2025-03-23 15:47:18 +0200

    Added an ugly modal window for adding student with not working functionality

--- script.js
+++ script.js
@@ -38,3 +38,45 @@ document
     document.getElementById("4-table-edit-btn").disabled = !this.checked;
     document.getElementById("4-table-delete-btn").disabled = !this.checked;
   });
+
+function showAddStudentDialog() {
+  document.getElementById("add-modal-window").style.display = "block";
+}
+
+function closeAddStudentDialog() {
+  document.getElementById("add-modal-window").style.display = "none";
+
+  document.getElementById("modal-group-input").value = "";
+  document.getElementById("modal-first-name-input").value = "";
+  document.getElementById("modal-last-name-input").value = "";
+}
+
+function addStudentToTheTable() {
+  let table = document.getElementById("students-table");
+  let tbody = table.getElementsByTagName("tbody")[0];
+
+  if (!tbody) {
+    console.error("Таблиця не має <tbody>!");
+    return;
+  }
+
+  let newRow = tbody.insertRow();
+
+  /*let groupCell = newRow.insertCell(1);
+  let nameCell = newRow.insertCell(2);
+  let genderCell = newRow.insertCell(3);
+  let birthdayCell = newRow.insertCell(4);
+  let statusCell = newRow.insertCell(5);
+  let optionsCell = newRow.insertCell(6);
+
+  groupCell.textContent = document.getElementById("modal-group-input").value;
+  nameCell.textContent =
+    document.getElementById("modal-first-name-input").value +
+    " " +
+    document.getElementById("modal-last-name-input").value;
+  genderCell =
+    document.getElementById("modal-gender-input").value == "Male" ? "M" : "F";
+  birthdayCell = document.getElementById("modal-birthday-input").value;*/
+
+  document.getElementById("add-modal-window").style.display = "none";
+}
