eee7c714b49b7f97f0bdf65057bbeb2241e374d5
Maksym <shalamax11@gmail.com>
2025-03-24 11:38:46 +0200

    Added possibility to add a new student

--- script.js
+++ script.js
@@ -55,28 +55,69 @@ function addStudentToTheTable() {
   let table = document.getElementById("students-table");
   let tbody = table.getElementsByTagName("tbody")[0];
 
-  if (!tbody) {
-    console.error("Таблиця не має <tbody>!");
-    return;
-  }
-
   let newRow = tbody.insertRow();
+  newRow.style.height = "2.5rem";
 
-  /*let groupCell = newRow.insertCell(1);
-  let nameCell = newRow.insertCell(2);
-  let genderCell = newRow.insertCell(3);
-  let birthdayCell = newRow.insertCell(4);
-  let statusCell = newRow.insertCell(5);
-  let optionsCell = newRow.insertCell(6);
+  let checkBoxCell = newRow.insertCell(0);
 
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkBoxCell.appendChild(checkbox);
+
+  let groupCell = newRow.insertCell(1);
   groupCell.textContent = document.getElementById("modal-group-input").value;
+
+  let nameCell = newRow.insertCell(2);
   nameCell.textContent =
     document.getElementById("modal-first-name-input").value +
     " " +
     document.getElementById("modal-last-name-input").value;
-  genderCell =
+
+  let genderCell = newRow.insertCell(3);
+  genderCell.textContent =
     document.getElementById("modal-gender-input").value == "Male" ? "M" : "F";
-  birthdayCell = document.getElementById("modal-birthday-input").value;*/
+
+  let birthdayCell = newRow.insertCell(4);
+  let birthdayValue = document.getElementById("modal-birthday-input").value;
+  let birthdayFormatted = birthdayValue
+    ? birthdayValue.split("-").reverse().join(".")
+    : "N/A";
+  birthdayCell.textContent = birthdayFormatted;
+
+  let statusCell = newRow.insertCell(5);
+  let status = document.createElement("div");
+  status.className = "circle-status offline";
+  statusCell.appendChild(status);
+
+  // Creating an options cell fillment
+  let optionsCell = newRow.insertCell(6);
+  let optionsDiv = document.createElement("div");
+  optionsDiv.className = "table-options";
+
+  let editButton = document.createElement("button");
+  editButton.className = "edit-table-button";
+  editButton.id = "new-table-edit-btn";
+  editButton.disabled = true;
+
+  let editImg = document.createElement("img");
+  editImg.src = "img/edit.svg";
+  editImg.alt = "edit";
+  editButton.appendChild(editImg);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.className = "delete-table-button";
+  deleteButton.id = "new-table-delete-btn";
+  deleteButton.disabled = true;
+
+  let deleteImg = document.createElement("img");
+  deleteImg.src = "img/delete.svg";
+  deleteImg.alt = "delete";
+  deleteButton.appendChild(deleteImg);
+
+  optionsDiv.appendChild(editButton);
+  optionsDiv.appendChild(deleteButton);
+
+  optionsCell.appendChild(optionsDiv);
 
   document.getElementById("add-modal-window").style.display = "none";
 }
